ca44db7534de5b8f089539a66179b0c9a8d8d9c2
Georgina Gorlero <georgina.gorlero@gmail.com>
2021-10-03 19:15:06 -0300

    Agregamos algunas librerías útiles y cambiamos las bases con las que comenzamos por unas más amigables en tamaño

--- Pruebas TP1.R
+++ Pruebas TP1.R
@@ -1,2 +1,34 @@
 
+library(tidyverse)
+library(janitor)
+library(stringr)
+library(vroom)
+library(lubridate)
+
+
+recorridos_2019 <- vroom("data/recorridos-realizados-2019.csv")
+recorridos_2020 <- vroom("data/recorridos-realizados-2020.csv")
+
+colnames(recorridos_2019)
+
+recorridos_2019_fechas <- recorridos_2019 %>% 
+                          mutate(mes=month(recorridos_2019$fecha_origen_recorrido), dia=day(recorridos_2019$fecha_origen_recorrido))
+
+recorridos_2019_jun <- recorridos_2019_fechas %>%
+                               filter(mes=="6", dia>14 & dia<=30) %>%
+                               select(-mes, -dia, -periodo, -id_usuario, -genero_usuario)
+
+write.csv(recorridos_2019_jun, "data/recorridos_2019_jun15_30.cvs")
+  
+class(recorridos_2019$fecha_origen_recorrido)
+
+recorridos_2020_fechas <- recorridos_2020 %>% 
+                          mutate(mes=month(recorridos_2020$fecha_origen_recorrido),dia=day(recorridos_2020$fecha_origen_recorrido))
+
+recorridos_2020_jun <- recorridos_2020_fechas %>%
+                       filter(mes=="6", dia>14 & dia<=30) %>%
+                       select(-mes, -dia, -periodo, -id_usuario)
+  
+
+write.csv(recorridos_2020_jun, "data/recorridos_2020_jun15_30.cvs")
 
